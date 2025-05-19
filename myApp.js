a5ab8a72c86ee0fb45a2ad98ad3740adee93d27f
djid-dev <116326083+djid-dev@users.noreply.github.com>
2025-05-18 23:27:08 -0400

    feat: add body-parser middleware and improve JSON response formatting

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+const bodyParser = require("body-parser")
 
 let express = require("express");
 let app = express();
 
 console.log("Hello World!");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
@@ -22,13 +25,37 @@ app.get("/json", (req, res) => {
 
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = "Hello json".toUpperCase();
-  }else{
+  } else {
     message = "Hello json";
   }
 
   res.json({
-    "message": message,
+    message: message,
   });
 });
+
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+});
+app.get("/now", (req, res) => {
+  res.json({
+    time: req.time,
+  });
+});
+
+app.get("/:word/echo", (req, res) =>
+  res.json({
+    echo: req.params.word,
+  })
+);
+
+app.route("/name").get( (req, res) => {
+  const first = req.query.first;
+  const last = req.query.last;
+  res.json({
+    name: `${first} ${last}`,
+  });
+})
 
 module.exports = app;
