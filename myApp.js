dbb44d9c24a06a3b665db189edf423a29db743e6
djid-dev <116326083+djid-dev@users.noreply.github.com>
2025-05-18 21:59:36 -0400

    feat: add logging middleware and update dotenv dependency to version 16.5.0

--- myApp.js
+++ myApp.js
@@ -1,12 +1,33 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 console.log("Hello World!");
+
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   const absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
 }
 );
+
+app.get('/json', (req, res) => {
+  let message = "Hello json";
+  
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({
+    "message":message
+  })
+})
 
  module.exports = app;
