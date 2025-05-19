5ab1de4c32e2287009b7bcf2d0c7f0a8b344184d
djid-dev <116326083+djid-dev@users.noreply.github.com>
2025-05-18 22:35:08 -0400

    fix: standardize quotation marks and formatting in myApp.js

--- myApp.js
+++ myApp.js
@@ -1,33 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
-let express = require('express');
+let express = require("express");
 let app = express();
 
 console.log("Hello World!");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
-})
+});
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-  const absolutePath = __dirname + '/views/index.html';
+app.get("/", (req, res) => {
+  const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
-}
-);
+});
 
-app.get('/json', (req, res) => {
-  let message = "Hello json";
-  
+app.get("/json", (req, res) => {
+  let message;
+
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
+    message = "Hello json".toUpperCase();
+  }else{
+    message = "Hello json";
   }
 
   res.json({
-    "message":message
-  })
-})
+    "message": message,
+  });
+});
 
- module.exports = app;
+module.exports = app;
